8d0053b8d7534cada426c4d106c11d6bd6b21973
ksmort <kierstenmort@gmail.com>
2015-11-08 18:39:15 -0700

    Create Lab.java

---
+++ Lab.java
@@ -0,0 +1,43 @@
+//Idea for project.  Feel free to modify or ignore.  Nov. 8, 2015
+
+//Method Lab that extends Class. 
+public class Lab extends Class {
+	String tutLab = "";
+	String tutLabNum = "";
+	
+	//Constructor where does not have additional arguments.
+	public Lab(String name, String ID, String lec, String lecNum) {
+				super(name, ID, lec, lecNum);
+	}
+
+	//Constructor with additional arguments for tutorial lab and tutorial lab number.
+	public Lab(String name, String ID, String lec, String lecNum, String tutLab, String tutLabNum) {
+		super(name, ID, lec, lecNum);
+		//tutLab and tutLabNum are not null values, then set them to this.tutLab and this.tutLabNum.
+		if (tutLab != null && tutLabNum != null) {
+			this.tutLab = tutLab;
+			this.tutLabNum = tutLabNum;
+		}
+	}
+	//Get tutLab.
+	public String getTutLab() {
+		return tutLab;
+	}
+	//Get tutLabNum.
+	public String getTutLabNum() {
+		return tutLabNum;
+	}
+	
+	//Labs are not courses, so return false.
+	@Override
+	public boolean isCourse() {
+		return false;
+	}
+
+	//Returns as full statement.  ex. CPSC 433 LEC 01 TUT 03
+	@Override
+	public String toString() {
+		return String.format("%s %s %s %s %s %s", name, ID, lec, lecNum, tutLab, tutLabNum);
+	}
+
+}
