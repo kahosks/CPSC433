0fda11e6330959999dd48f364600aa38bd5df8fd
lucmichaud <85lucmichaud@gmail.com>
2015-11-21 12:14:31 -0700

    add some documentation

--- ParserTest.java
+++ ParserTest.java
@@ -5,32 +5,23 @@ import org.junit.Test;
 
 public class ParserTest {
 
+	// Test to see if the parser loads a proper file, pass if no error thrown
 	@Test
 	public void TestParserConstruct() throws SchedulerException {
 		Parser parser = new Parser("ShortExample");
 	}
 	
+	// Test loading a file that doesn't exist, should throw an error
 	@Test(expected = SchedulerException.class)
 	public void TestParserConstructFileDoesntExist() throws SchedulerException {
 		Parser parser = new Parser("abc.def");
 	}
 	
+	// Test the parser with the example given, pass if no errors are thrown
 	@Test
 	public void TestParse() throws SchedulerException {
 		Parser parser = new Parser("ShortExample");
 		parser.parse();
 		Vector<Class> labsAndCourses =  parser.getLabsAndCourses();
-		
-		//parser.parse();
 	}
-	
-//	@Test
-//	public void TestGetLabsAndCourses() throws SchedulerException {
-//		Parser parser = new Parser("ShortExample");
-//		parser.parse();
-//		
-//		Vector<Class> labsAndCourses =  parser.getLabsAndCourses();
-//		
-//		Assert.assertArrayEquals(, labsAndCourses);
-//	}
 }
