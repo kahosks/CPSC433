1da12df004b6cd6000413a0e04c342f175c8187f
ksmort <kierstenmort@gmail.com>
2015-11-27 13:21:22 -0700

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,8 +1,10 @@
+/**
+ * Class Driver that instantiates the Scheduler class which runs the program.
+ */
 public class Driver{
 
-	public static void main(String[] args) {
-	
-		//stuff
-		
+	public static void main(String[] args) throws SchedulerException{
+		Scheduler sch = new Scheduler();
+		sch.start(args);
 	}
 }
