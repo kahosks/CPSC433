c01041903a8197108dc92ccc587f63313e28188e
lucmichaud <85lucmichaud@gmail.com>
2015-12-03 12:06:32 -0700

    Update CommandParserTest.java
    Fixed to work with the removal of pen_pref from the command parser

--- CommandParserTest.java
+++ CommandParserTest.java
@@ -17,17 +17,17 @@ public class CommandParserTest {
 	public void testInput() throws SchedulerException { 
 		String[] input = "-f bob.txt -m 3 -3 -pr 4 -4 -pa 5 -5 -s 6 -6".split(" ");
 		CommandParser cp = new CommandParser(input);
-		int[] actual = new int[8];
+		int[] actual = new int[7];
 		actual[0] = cp.getMinfilled();
 		actual[1] = cp.getPenMinfilled();
 		actual[2] = cp.getPref();
-		actual[3] = cp.getPenPref();
-		actual[4] = cp.getPair();
-		actual[5] = cp.getPenPair();
-		actual[6] = cp.getSecdiff();
-		actual[7] = cp.getPenSecdiff();
+		//actual[3] = cp.getPenPref();
+		actual[3] = cp.getPair();
+		actual[4] = cp.getPenPair();
+		actual[5] = cp.getSecdiff();
+		actual[6] = cp.getPenSecdiff();
 		
-		int[] expected = {3, -3, 4, -4, 5, -5,6, -6};
+		int[] expected = {3, -3, 4,/* -4,*/ 5, -5, 6, -6};
 
 		Assert.assertArrayEquals(expected, actual);
 		
