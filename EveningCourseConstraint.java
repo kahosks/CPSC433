45db6bbca9445a3fe3c8e7da756789639b655720
Kneavel <christopherdneave@gmail.com>
2015-12-02 21:36:14 -0700

    Evening course hard constraint

---
+++ EveningCourseConstraint.java
@@ -0,0 +1,18 @@
+/**
+ * EveningConstraint is made for when a lecture can only take place in the evenings
+ * @author cdneave
+ *
+ */
+public class EveningCourseConstraint extends Constraint{
+	private int index;	//Index of the evening course
+	
+	@Override
+	public boolean testHard(int[] node) {
+		if(node[index]%2400 >= 1800){return true;}//If the class starts after 6pm (1800)
+		return false;
+	}
+
+	public EveningCourseConstraint(int indexOfCourse){
+		this.index = indexOfCourse;
+	}
+}
