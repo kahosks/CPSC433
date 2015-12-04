db693b472711e7e0c15838b6df57d59b86a0028b
Kneavel <christopherdneave@gmail.com>
2015-12-04 11:22:51 -0700

    made it work

--- OverlapConstraint.java
+++ OverlapConstraint.java
@@ -6,18 +6,32 @@ public class OverlapConstraint extends Constraint{
 	private int indexLab;
 	@Override
 	public boolean testHard(int[] node) {
+		//System.out.println("Course time:" + node[indexCourse] + " Lab time: " + node[indexLab] );
+		//If either slot is still unassigned
+		if(node[indexCourse] == 0 || node[indexLab]==0){return true;}	//Return that they do not overlap
+		
 		//Takes care of M/W and Tuesday on the hour
 		if(node[indexCourse]==node[indexLab]){return false;}
 		
-		//Takes care of Tu/Th 
-		if(node[indexCourse]+70==node[indexLab]){return false;}			//Labs starting on the hour after the half hours
-		else if(node[indexCourse]-30 == node[indexLab]){return false;}	//Labs starting on the hour before the half hours
-		else if(node[indexCourse]+100 == node[indexLab]){return false;}	//Labs starting on the hour after the even hours
+		if(node[indexLab]>2400){
+			//Takes care of Tu/Th 
+			if(node[indexCourse]+70==node[indexLab]){return false;}			//Labs starting on the hour after the half hours
+			else if(node[indexCourse]-30 == node[indexLab]){return false;}	//Labs starting on the hour before the half hours
+			else if(node[indexCourse]+100 == node[indexLab]){return false;}	//Labs starting on the hour after the even hours
 		
-		//Takes care of F
-		int fridayConvert = node[indexLab]-4800;
-		if(node[indexCourse]+100 == fridayConvert){return false;}		//Labs starting on the hour after the course on fridays
+			//Takes care of F
+			int fridayConvert = node[indexLab]-4800;
+			if(node[indexCourse]+100 == fridayConvert){return false;}		//Labs starting on the hour after the course on fridays
+		}
 		return true;
 	}
 
+	public OverlapConstraint(int indexCourseIn, int indexLabIn){
+		this.indexCourse = indexCourseIn;
+		this.indexLab = indexLabIn;
+	}
+	
+	public String toString(){
+		return "OverlapConstratint:" + indexCourse + "labIndex:"+ indexLab;
+	}
 }
